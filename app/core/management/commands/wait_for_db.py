7339a5e78fa0aa95d02e21f270836d8ec314282a
Gael Zamora <jgaelhz@gmail.com>
2024-08-15 18:41:40 -0600

    fixed blank lines in project

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,12 +9,9 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     """Django command to wait for database"""
-
     def handle(self, *args, **options):
         """Entrypoint for command"""
-
         self.stdout.write('Waiting for database...')
-
         db_up = False
         while db_up is False:
             try:
