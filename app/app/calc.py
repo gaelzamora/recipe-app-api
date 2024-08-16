b10292313eb282e7f025283b5d4800bb92e44d16
Gael Zamora <jgaelhz@gmail.com>
2024-08-15 18:50:57 -0600

    fixed blank lines in project

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,9 @@ Calculator functions
 
 def add(x, y):
     """Add x and y and return result."""
-    return x+y
+    return x + y
+
 
 def subtract(x, y):
-    """Subtract x from y and return Result."""
-
-    return 5
+    """Subtract x from y and return result."""
+    return x - y  # Este retorno fue ajustado para que sea coherente
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,15 +3,16 @@ Sample test
 """
 
 from django.test import SimpleTestCase
-
 from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
+
     def test_add_number(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
